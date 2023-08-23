27a8edbb7a6f37a5ccebb9d34272adaa525755fb
Yohana-Gutierrez <137759423+Yohana-Gutierrez@users.noreply.github.com>
2023-08-22 23:43:53 -0600

    Add files via upload

---
+++ SimpletronNew/src/simpletronnew/SimpletronNewPrueba.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simpletronnew;
+
+/**
+ *
+ * @author Anny Yohana
+ */
+public class SimpletronNewPrueba {
+	public static void main ( String args [] ) 
+	{
+		SimpletronNew simulator = new SimpletronNew ( );
+		simulator.CorrerSimulator ();
+
+	}	// Fin metodo main
+
+}	// end class SimpletronNewPrueba
+   
